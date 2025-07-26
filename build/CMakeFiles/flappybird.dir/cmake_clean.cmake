3a70540429a91e072269fd773c6321d71d0895cc
jblanco081 <julian.blanco@mykenzie.snhu.edu>
2025-07-25 20:42:34 -0500

    Got the game working, thinking of adding a menu or quicker restart

---
+++ build/CMakeFiles/flappybird.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flappybird.dir/main.cpp.obj"
+  "CMakeFiles/flappybird.dir/main.cpp.obj.d"
+  "flappybird.exe"
+  "flappybird.exe.manifest"
+  "flappybird.pdb"
+  "libflappybird.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/flappybird.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
